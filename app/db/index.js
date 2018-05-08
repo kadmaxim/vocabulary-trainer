d578220bb3f6538a6ae1bb6239b8cf6a9e10d5d7
Maksym Kadatskyi <m.kadatskyi@codeblick.de>
2018-05-09 00:48:26 +0200

    Add mock DB

---
+++ app/db/index.js
@@ -0,0 +1,45 @@
+
+export default [
+  {
+    'id' : 1,
+    'original' : 'entwickeln',
+    'translation': 'develop',
+    'img_url': 'https://fifocapital.com.au/wp-content/uploads/2015/08/Develop-your-team.jpg',
+  },
+  {
+    'id' : 2,
+    'original' : 'ernst',
+    'translation': 'serious',
+    'img_url': 'https://media.tenor.com/images/1c17a961d2949d1292d87ebc1b5d3fa0/tenor.png',
+  },
+  {
+    'id' : 3,
+    'original' : 'eine Birne',
+    'translation': 'a pear',
+    'img_url': 'https://pagewizz.com/static/uploads/de/module/image/2014/09/05/15-39-31-829_294.jpg',
+  },
+  {
+    'id' : 4,
+    'original' : 'entschuldigen',
+    'translation': 'apologise',
+    'img_url': 'http://wienerin.at/images/uploads_610/9/a/c/4975020/shutterstock397786363146158341228923_h.jpg',
+  },
+  {
+    'id' : 5,
+    'original' : 'die Eigentum',
+    'translation': 'the property',
+    'img_url': 'https://www.advopedia.de/var/advopedia/storage/images/rechtslexikon/eigentum/53930-1-ger-DE/eigentum_ng_image_full.png',
+  },
+  {
+    'id' : 6,
+    'original' : 'cold',
+    'translation': 'kalt',
+    'img_url': 'https://www.schulbilder.org/malvorlage-kalt-dm11753.jpg',
+  },
+  {
+    'id' : 7,
+    'original' : 'der Regen',
+    'translation': 'the rain',
+    'img_url': 'https://events24-agenturbelmediag.netdna-ssl.com/wp-content/uploads/2014/05/Regen-Smit-Shutterstock.com_.jpg',
+  },
+]
