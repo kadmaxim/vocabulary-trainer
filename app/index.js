ecf89db0880f4ed0532b6eac31370569dee90d5b
Maksym Kadatskyi <maksym.kadatskyi@hs-augsburg.de>
2018-05-27 14:23:40 +0300

    Update server settings

--- app/index.js
+++ app/index.js
@@ -1,6 +1,33 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const url = require('url');
+const session = require('express-session');
+const passport = require('passport');
+const cookieParser = require('cookie-parser');
+const multer = require('multer');
+
+const PORT = process.env.PORT || 3000;
+
 const app = express();
 
-app.get('/', (req, res) => res.send('Hello World!'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended : true }));
+app.use(multer().array());
+app.use(cookieParser());
+app.use(express.static('public'));
 
-app.listen(3000, () => console.log('Example app listening on port 3000!'));
+app.set('port', PORT);
+app.use(session({
+    secret : 'vocabulary-trainer',
+    resave : false,
+    saveUninitialized : true,
+    cookie : { maxAge : 60000 * 12 }
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+require('./api/config/passport')(passport);
+require('./api/routes')(app, passport);
+
+app.listen(app.get('port'), () => console.log(`Server is running on port ${app.get('port')}`));
