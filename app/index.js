67970e6eefe04a67b0d443627541120c44f3f605
Maksym Kadatskyi <maksym.kadatskyi@hs-augsburg.de>
2018-05-27 14:14:24 +0200

    Bind React app with server API

--- app/index.js
+++ app/index.js
@@ -5,24 +5,27 @@ const session = require('express-session');
 const passport = require('passport');
 const cookieParser = require('cookie-parser');
 const multer = require('multer');
+const path = require('path');
 
 const PORT = process.env.PORT || 3000;
 
 const app = express();
 
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended : true }));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(multer().array());
 app.use(cookieParser());
-app.use(express.static('public'));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.set('port', PORT);
-app.use(session({
-    secret : 'vocabulary-trainer',
-    resave : false,
-    saveUninitialized : true,
-    cookie : { maxAge : 60000 * 12 }
-}));
+app.use(
+  session({
+    secret: 'vocabulary-trainer',
+    resave: false,
+    saveUninitialized: true,
+    cookie: { maxAge: 60000 * 12 }
+  })
+);
 
 app.use(passport.initialize());
 app.use(passport.session());
@@ -30,4 +33,6 @@ app.use(passport.session());
 require('./api/config/passport')(passport);
 require('./api/routes')(app, passport);
 
-app.listen(app.get('port'), () => console.log(`Server is running on port ${app.get('port')}`));
+app.listen(app.get('port'), () =>
+  console.log(`Server is running on port ${app.get('port')}`)
+);
